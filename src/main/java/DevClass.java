ba069fb957bd9bb74c4930c351c3752e0decfbf2
Tejan_Gunjal <tejangunjal2@gmail.com>
2022-05-19 16:52:49 +0530

    Initial Commit

---
+++ src/main/java/DevClass.java
@@ -0,0 +1,6 @@
+public class DevClass {
+
+    public static void main(String[] args) {
+        System.out.println("Inside DevClass");
+    }
+}
---
+++ src/test/java/DevTest.java
@@ -0,0 +1,7 @@
+public class DevTest {
+
+    public static void main(String[] args) {
+        System.out.println("Inside DevTestClass");
+    }
+}
+

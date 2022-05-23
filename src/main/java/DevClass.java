44ec52b69cdae5c477f32c06038ebc68c88fe48e
Tejan_Gunjal <tejangunjal2@gmail.com>
2022-05-23 12:59:45 +0530

    Added print

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,5 +2,7 @@ public class DevClass {
 
     public static void main(String[] args) {
         System.out.println("Inside DevClass");
+        System.out.println("Added to check stash works");
+        System.out.println("Need to check");
     }
 }
